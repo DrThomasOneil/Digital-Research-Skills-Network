55eee97ecbd480d7bbb01b44411c6e3d610e41e0
DrThomasOneil <thomas.oneil@sydney.edu.au>
2025-01-06 11:49:04 +1100

    InitialDeployment

---
+++ docs/r-tutorial/adit/quotes.js
@@ -0,0 +1,103 @@
+const quotes = [
+    {
+      quote: "And still we will be here, standing like statues.",
+      song: "Stand Your Ground; This Is Ancient Land",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "The spark ignites within us, and we can't turn back.",
+      song: "The Spark",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "We're not the same, but we're not that different.",
+      song: "Arguing With Thermometers",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "Destabilize, break down the walls.",
+      song: "Destabilize",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "I refuse to be a slave to the system.",
+      song: "System...",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "The walls of the cage are only in your mind.",
+      song: "Gandhi Mate, Gandhi",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "We are all one, beneath the surface.",
+      song: "Warm Smiles Do Not Make You Welcome Here",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "I am the spark, and you are the fire.",
+      song: "The Spark",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "The future's not what it used to be.",
+      song: "The Last Garrison",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "If I lower my defense, would you still believe?",
+      song: "Undercover Agents",
+      artist: "Enter Shikari"
+    },
+    {
+      quote: "I'm still searching for the answers.",
+      song: "Lucid",
+      artist: "Polaris"
+    },
+    {
+      quote: "Consumed by shadows and swallowed by fear.",
+      song: "Consume",
+      artist: "Polaris"
+    },
+    {
+      quote: "When you feel the fire burn, let it light your way.",
+      song: "Dusk to Day",
+      artist: "Polaris"
+    },
+    {
+      quote: "This weight is pulling me down.",
+      song: "Casualty",
+      artist: "Polaris"
+    },
+    {
+      quote: "You can't break what's already broken.",
+      song: "Relapse",
+      artist: "Polaris"
+    },
+    {
+      quote: "I won't let you fade away.",
+      song: "The Remedy",
+      artist: "Polaris"
+    },
+    {
+      quote: "We build these walls to watch them crumble.",
+      song: "The Mortal Coil",
+      artist: "Polaris"
+    },
+    {
+      quote: "I don't want to fade away into the night.",
+      song: "Lucid",
+      artist: "Polaris"
+    },
+    {
+      quote: "I'm just a shadow of who I used to be.",
+      song: "Consume",
+      artist: "Polaris"
+    },
+    {
+      quote: "The darkness won't define me.",
+      song: "Dusk to Day",
+      artist: "Polaris"
+    }
+  ];
+  
--- workflow.R
+++ workflow.R
@@ -3,13 +3,14 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 wflow_status()
 
-wflow_build("analysis/index.Rmd")
+wflow_build("analysis/*.Rmd")
 
 #
 gc()
 #
 #
-#wflow_publish(c("analysis/*"),"Update")
+
+wflow_publish(c("analysis/*"),"Initial Deployment")
 
 wflow_status()
 wflow_view()
