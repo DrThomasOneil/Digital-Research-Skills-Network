40eec40bf655982f0df880cc2c3201f74c377c7c
DrThomasOneil <thomas.oneil@sydney.edu.au>
2025-05-07 10:26:24 +1000

    update

--- workflow.R
+++ workflow.R
@@ -1,10 +1,11 @@
+{
 library("workflowr")
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 wflow_status()
 files <- list.files("analysis", pattern = "\\.Rmd$", full.names = TRUE)
 files <- files[!grepl("^x", basename(files))]
-
+}
 wflow_build(files)
 wflow_publish(files)
 
