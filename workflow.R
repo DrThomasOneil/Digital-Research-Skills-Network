e7ac7149ef8ea3aa4a7e3252694d613574d159cc
DrThomasOneil <thomas.oneil@sydney.edu.au>
2026-08-04 11:12:44 +1000

    update

--- workflow.R
+++ workflow.R
@@ -6,7 +6,7 @@ wflow_status()
 files <- list.files("analysis", pattern = "\\.Rmd$", full.names = TRUE)
 files <- files[!grepl("^x", basename(files))]
 }
-wflow_publish(c("analysis/0_schedule.rmd"))
+wflow_publish(c("analysis/index.Rmd", "analysis/202608.Rmd", "analysis/0_schedule.Rmd"))
 
 wflow_status()
 wflow_view()
